50c87fdffdc23951eab91126662b9d2b23df651f
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-26 11:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const apiUrls = [
 ];
 
 // You can write your code here
+let promises = new Array(5);
+
+for (let i = 0; i < promises.length; i++) {
+      promises[i] = fetch(apiUrls[i]);
+}
+
+let outputAll = document.getElementById('output-all');
+
+let d1 = new Date();
+Promise.all(promises).then((res) =>{
+  let d2 = new Date();
+  outputAll.innerText = d2-d1;
+})
+
+let outputAny = document.getElementById('output-any');
+let d3 = new Date();
+Promise.any(promises).then((res)=>{
+  let d4 = new Date();
+  outputAny.innerText = d4-d3;
+})
